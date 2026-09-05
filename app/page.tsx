1bbe63c2f79d588cf96fdbbba36d7b5285fe514e
PhanHoangKhang <togaphan@gmail.com>
2026-09-05 18:23:34 +0700

    refactor: adjust the bg body background

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   );
 
   return (
-    <div className="min-h-screen flex flex-col bg-[var(--color-bg-light)]">
+    <div className="min-h-screen flex flex-col">
 
       <main className="flex-1">
         <Category />
